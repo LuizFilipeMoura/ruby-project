1e108c18d4b5787fb607f5fd89d2875a7cbe4956
Luiz Filipe <luizfilipeholiday@gmail.com>
2024-12-20 19:29:36 -0300

    a

---
+++ server/main.ts
@@ -0,0 +1,27 @@
+import { serve } from "https://deno.land/std@0.220.1/http/server.ts";
+import { Server } from "https://deno.land/x/socket_io@0.2.0/mod.ts";
+
+const io = new Server();
+
+io.on("connection", (socket) => {
+  console.log(`socket ${socket.id} connected`);
+
+  Object.keys(handlers).forEach((eventName) => {
+    socket.on(eventName, (data) => {
+      try {
+        handlers[eventName](socket, data);
+      } catch (error) {
+        console.error(`Error handling event '${eventName}':`, error);
+      }
+    });
+  });
+  socket.emit("hello", "world");
+
+  socket.on("disconnect", (reason) => {
+    console.log(`socket ${socket.id} disconnected due to ${reason}`);
+  });
+});
+
+await serve(io.handler(), {
+  port: 3000,
+});
